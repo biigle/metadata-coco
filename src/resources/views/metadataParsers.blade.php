b8bf1e05f7aabde45025df43be7dcad26a4bbc1f
Martin Zurowietz <martin@zurowietz.de>
2024-09-30 14:37:14 +0200

    Add manual article content

--- src/resources/views/metadataParsers.blade.php
+++ src/resources/views/metadataParsers.blade.php
@@ -1,3 +1,3 @@
 <li>
-    <strong>COCO:</strong> TODO
+    <strong>COCO:</strong> This file format is often used in machine learning applications. You can use it to import automatically generated annotations into BIIGLE for further exploration or processing. A detailed description of the format can be found <a href="https://cocodataset.org/#format-data">here</a>.
 </li>
